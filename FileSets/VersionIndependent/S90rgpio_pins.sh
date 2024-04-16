298da73a1461e0ebf73d1b38f94c1387c93b9c2a
Derrick <120696745+drtinaz@users.noreply.github.com>
2024-04-16 16:50:14 -0700

    update S90pins

--- FileSets/VersionIndependent/S90rgpio_pins.sh
+++ FileSets/VersionIndependent/S90rgpio_pins.sh
@@ -11,7 +11,7 @@
 
 # Kill existing rgpio_service in case the script is called after HW configuration change:
 #kill $(ps | grep '{rgpio_monitor}' | grep -v grep | awk '{print $1}') 2>/dev/null
-svc -d /service/rgpio_monitor
+#svc -d /service/rgpio_monitor
 
 get_setting()                                                                                                                                                                                                  
     {                                                                                                                                                                                                      
@@ -362,8 +362,8 @@ fi
 #Service
 svc -t /service/dbus-systemcalc-py
 svc -t /service/dbus-digitalinputs
-svc -u /service/rgpio_monitor
-svc -t /service/start-gui
+#svc -u /service/rgpio_monitor
+#svc -t /service/start-gui
 #[ ! -f /service/rgpio ] && ln -sf /data/RemoteGPIO/service/rgpio /service/rgpio
 
 #For managing reboot of Dingtian IOT devices
